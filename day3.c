3acdfd07d5ff963005bfdaf42c75175acb694640
Leonardo Mazzone <leo@mazzone.space>
2025-12-03 16:02:14 +0000

    Solve second part of day 3

--- day3.c
+++ day3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #define MAX_BLOCK_SIZE 1000
+#define ROUNDS 12
 
 
 int main() {
@@ -8,50 +9,54 @@ int main() {
     if (fptr == NULL) printf("Input could not be opened.");
 
     long solution = 0;
-    char block[MAX_BLOCK_SIZE+1];
-    while (fgets(block, sizeof(block), fptr)) {
-        printf("%s", block);
-        int max = -1;
-        int max_i;
-        int max2 = -1;
-        int max2_i;
-        int num;
-        int i = 0;
+    char block_string[MAX_BLOCK_SIZE+1];
 
+    while (fgets(block_string, sizeof(block_string), fptr)) {
+        int block[MAX_BLOCK_SIZE];
+        int length = 0;
 
-        while (block[i] != '\n') {
-            num = block[i] - '0';
-            if (num > max && block[i+1] != '\n') {
-                max = num;
-                max_i = i;
+        // Read one line into an array of int
+        for (int i = 0; block_string[i] != '\0' && block_string[i] != '\n'; i++) {
+            block[length] = block_string[i] - '0';
+            length++;
+        }
+
+        int max[ROUNDS];
+        int position = 0;
+        // Each round finds one battery to switch on
+        // We choose digits left to right. We give maximum priority to
+        // the current digit at each round, as long as there are enough
+        // digits left for the remaining rounds
+        for (int round=0; round<ROUNDS; round++) {
+            max[round] = 0;
+            for (int i=position; i<length-(ROUNDS-1-round); i++) {
+                if (block[i] > max[round]) {
+                    max[round] = block[i];
+                    position = i + 1;
+                }
             }
-            i++;
         }
 
-        i = max_i+1;
-        while (block[i] != '\n') {
-            num = block[i] - '0';
-            if (num > max2) {
-                max2 = num;
-                max2_i = i;
-            }
+        printf("%s", block_string);
 
-            i++;
+        long power = 1;
+        long joltage = 0;
+        // Once you have all the digits, you need to reconstruct the joltage (sic)
+        // which I do by summing each digit times its significance as a power of 10
+        for (int i=ROUNDS-1; i>=0; i--) {
+            joltage += power * max[i];
+            power *= 10;
 
         }
+        printf("%ld", joltage);
+        printf("\n\n");
+        solution += joltage;
 
-        printf("max: %d, max2: %d \n", max, max2);
-        if (max_i < max2_i) {
-            printf("%d%d\n", max, max2);
-            solution += 10*max + max2;
-        } else {
-            printf("%d%d\n", max2, max);
-            solution += 10*max2 + max;
-        }
-        printf("\n");
+
     }
 
-    printf("Solution: %ld", solution);
+
+    printf("Solution: %ld\n", solution);
 
     fclose(fptr);
 
