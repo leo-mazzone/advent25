38fbe9de3a14f0a2f82d4d3b82de1ae0de485c87
Leonardo Mazzone <leo@mazzone.space>
2025-12-04 08:44:40 +0000

    Solve day 4 in a naive way

--- day4_naive.c
+++ day4_naive.c
@@ -4,10 +4,13 @@
 #define MAX_COLS 1000
 #define MAX_ROWS 1000
 
-void process_line(char* buffer, int n_cols, int* n_rows, int** grid) {
+void process_line(char* buffer, int n_cols, int* n_rows, int** grid, int** next_grid) {
     grid[*n_rows] = malloc(n_cols * sizeof(int));
+    next_grid[*n_rows] = malloc(n_cols * sizeof(int));
     for (int i=0; buffer[i] != '\0'; i++) {
-        grid[*n_rows][i] = buffer[i] == '@' ? 1 : 0;
+        int value = buffer[i] == '@' ? 1 : 0;
+        grid[*n_rows][i] = value;
+        next_grid[*n_rows][i] = value;
     }
     (*n_rows)++;
 }
@@ -17,8 +20,9 @@ int valid_coordinates(int r, int c, int n_rows, int n_cols) {
     return 1;
 }
 
-int count_removable(int n_rows, int n_cols, int** grid) {
+int remove_paper(int n_rows, int n_cols, int** grid, int** next_grid) {
     int removable = 0;
+    // Iterate over cells
     for (int r=0; r<n_rows; r++) {
         for (int c=0; c<n_cols; c++) {
             int total = 0;
@@ -34,7 +38,10 @@ int count_removable(int n_rows, int n_cols, int** grid) {
                 }
             }
 
-            if (total < 4) removable++;
+            if (total < 4) {
+                next_grid[r][c] = 0;
+                removable++;
+            }
         }
     }
     return removable;
@@ -47,15 +54,16 @@ int main() {
     int n_rows = 0;
     int n_cols = 0;
     int* grid[MAX_ROWS];
+    int* next_grid[MAX_ROWS];
 
     char buffer[MAX_COLS];
     // Read first line to determine number of columns
     fgets(buffer, sizeof(buffer), fptr);
     n_cols = strlen(buffer) - 1;
-    process_line(buffer, n_cols, &n_rows, grid);
+    process_line(buffer, n_cols, &n_rows, grid, next_grid);
     // Read all remaining lines
     while (fgets(buffer, sizeof(buffer), fptr)) {
-        process_line(buffer, n_cols, &n_rows, grid);
+        process_line(buffer, n_cols, &n_rows, grid, next_grid);
     }
     // Print grid for sense-checking
     for (int r=0; r<n_rows; r++) {
@@ -67,12 +75,22 @@ int main() {
     printf("%d x %d\n", n_cols, n_rows);
 
     int solution = 0;
-    solution += count_removable(n_rows, n_cols, grid);
+    int removed;
+    do {
+        removed = remove_paper(n_rows, n_cols, grid, next_grid);
+        solution += removed;
+
+        // Copy next_grid back to grid
+        for (int r=0; r<n_rows; r++) {
+            memcpy(grid[r], next_grid[r], n_cols * sizeof(int));
+        }
+    } while (removed > 0);
     printf("The solution was %d\n", solution);
 
     // Clean up
     for (int i=0; i<n_rows; i++) {
         free(grid[i]);
+        free(next_grid[i]);
     }
     fclose(fptr);
 
